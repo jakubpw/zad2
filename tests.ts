3fc4b3c37f7382cb8bae2771d231e136df98e3de
jakubpw <jawalendowski@gmail.com>
2020-07-01 23:21:28 +0200

    test ok

--- tests.ts
+++ tests.ts
@@ -1,28 +1,27 @@
-import { Builder, By } from 'selenium-webdriver';
+import { Builder, By} from 'selenium-webdriver';
 import { driver } from 'mocha-webdriver';
 
 const test1 = async () => {
-    let driver = await new Builder().forBrowser("chrome").build();
+    let driver = await new Builder().forBrowser("firefox").build();
     await driver.get("http://localhost:3000");
     await driver.findElement(By.name("login")).sendKeys("user_1");
     await driver.findElement(By.name("password")).sendKeys("user_1");
-    await driver.find('input[type=submit]').doClick();
+    await (await driver.findElement(By.css('input[type=submit]'))).click();
     var cookies = await driver.manage().getCookies();
     await driver.manage().deleteAllCookies();
     await driver.get("http://localhost:3000");
     await driver.findElement(By.name("login")).sendKeys("user_1");
     await driver.findElement(By.name("password")).sendKeys("user_1");
-    await driver.find('input[type=submit]').doClick();
+    await (await driver.findElement(By.css('input[type=submit]'))).click();
     await driver.get("http://localhost:3000/change_password");
     await driver.findElement(By.name("new_password")).sendKeys("new_pass");
-    await driver.find('input[type=submit]').doClick();
+    await (await driver.findElement(By.css('input[type=submit]'))).click();
     await driver.get("http://localhost:3000");
-    await driver.manage().deleteAllCookies();
-    for (var cookie of cookies) {
-        await driver.manage().addCookie(cookie)
-    }
 }
 
 test1()
     .then((ret) => console.log('PASS'))
     .catch((err) => console.log('ERROR', err))
+
+
+    
